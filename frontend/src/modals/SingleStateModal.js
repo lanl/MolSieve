e51ba2a677a458799294bf9aee6380f7f5d84579
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2022-03-02 21:44:27 -0700

    Added info table to single state modal

--- frontend/src/modals/SingleStateModal.js
+++ frontend/src/modals/SingleStateModal.js
@@ -5,7 +5,13 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import Button from "@mui/material/Button";
 import {api_generate_ovito_image} from "../api/ajax";
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 class SingleStateModal extends React.Component {
     constructor(props) {
@@ -19,6 +25,7 @@ class SingleStateModal extends React.Component {
             .then((response) => {                
                 const imageData = 'data:image/png;base64,' + response.image;                
                 this.imgRef.current.setAttribute("src", imageData);
+                console.log(this.state);
             })
             .catch((e) => { alert(e);})        
     }
@@ -28,6 +35,15 @@ class SingleStateModal extends React.Component {
     };
 
     render() {
+
+        var tableRows = Object.keys(this.state.trajectoryState).map((attr) => {
+            return(
+            <TableRow key={attr}>
+                <TableCell>{attr}</TableCell>
+                <TableCell>{this.state.trajectoryState[attr]}</TableCell>
+            </TableRow>);
+        });        
+        
         return (<Dialog
                     onClose={this.closeFunc}
                     open={this.props.open}
@@ -38,6 +54,20 @@ class SingleStateModal extends React.Component {
                 <DialogTitle>State {this.state.trajectoryState.number}</DialogTitle>
                     <DialogContent>
                         <img ref={this.imgRef} />
+                        <br/>
+                        <TableContainer component={Paper}>
+                            <Table size="small">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Attribute name</TableCell>
+                                        <TableCell>Attribute value</TableCell>
+                                    </TableRow>                                
+                                </TableHead>
+                            <TableBody>
+                                {tableRows}
+                            </TableBody>
+                            </Table>
+                        </TableContainer>
                     </DialogContent>
                     <DialogActions>
                         <Button size="small" onClick={this.closeFunc}>Close</Button>
