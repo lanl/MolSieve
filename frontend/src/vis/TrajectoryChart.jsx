cbf71e2a04a9f93b9bce5fee5eeb72449d28daee
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2023-02-10 12:38:05 -0700

    Minor trajectoryChart optimization

--- frontend/src/vis/TrajectoryChart.jsx
+++ frontend/src/vis/TrajectoryChart.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
 import { useTrajectoryChartRender } from '../hooks/useTrajectoryChartRender';
@@ -56,6 +56,9 @@ export default function TrajectoryChart({
 
     const { ranks, reduceRanks } = useRanks(properties, trajectory.chunkOrder());
 
+    const [scales, setScales] = useState(null);
+    const [chartSelections, setChartSelections] = useState({});
+
     const updateRanks = (values, weight) => {
         reduceRanks({ type: 'updateValues', payload: { values, weight } });
     };
@@ -86,102 +89,63 @@ export default function TrajectoryChart({
         }
     }, [JSON.stringify(ranks.ordered), ref, showTop, propertyCombos, height]);
 
-    const { extents } = run;
-    // here we can filter out the un-rendered charts right away since we only care about rendering here
-    const { iChunks, uChunks } = charts;
+    useEffect(() => {
+        const { extents } = run;
+        const { iChunks, uChunks, topChunkList } = charts;
+        const unimportantWidthExtent =
+            iChunks.length > 0 ? (width - MARGIN.right) * 0.1 : width - MARGIN.right;
 
-    const topChunkList = [...iChunks, ...uChunks].sort((a, b) => a.timestep > b.timestep);
-
-    const unimportantWidthExtent =
-        iChunks.length > 0 ? (width - MARGIN.right) * 0.1 : width - MARGIN.right;
-
-    const unimportantWidthScale = d3
-        .scaleLinear()
-        .range([minimumChartWidth, unimportantWidthExtent])
-        .domain([0, d3.max(uChunks, (d) => d.size)]);
-
-    const importantWidthScale = d3
-        .scaleLinear()
-        .range([minimumChartWidth, width - MARGIN.right])
-        .domain([0, d3.max(iChunks, (d) => d.slice(extents[0], extents[1]).size)]);
-
-    const getWidthScale = (data) => {
-        if (data.important) {
-            return importantWidthScale(data.slice(extents[0], extents[1]).size);
-        }
-        return unimportantWidthScale(data.size);
-    };
-
-    const totalSum = d3.sum(topChunkList, (d) => getWidthScale(d));
-
-    const scaleX = (w) => {
-        // given a width, scale it down so that it will fit within 1 screen
-        const per = w / totalSum;
-        return per * (width - MARGIN.right);
-    };
-
-    const getX = (i, w) => {
-        if (i > 0) {
-            const d = topChunkList[i - 1];
-            const wl = scaleX(getWidthScale(d));
-            return getX(i - 1, w + wl);
-        }
-        return w;
-    };
-
-    const finishBrush = (chunk, { selection }, chartWidth) => {
-        // extents determines the zoom level
-        const [chunkSliceStart, chunkSliceEnd] = extents;
-
-        const x = d3
+        const unimportantWidthScale = d3
             .scaleLinear()
-            .domain(
-                d3.extent(chunk.timesteps.filter((d) => d >= chunkSliceStart && d <= chunkSliceEnd))
-            )
-            .range([0, chartWidth - 7.5]); // 7.5 to match margin on scatterplot
+            .range([minimumChartWidth, unimportantWidthExtent])
+            .domain([0, d3.max(uChunks, (d) => d.size)]);
 
-        const startTimestep = x.invert(selection[0]);
-        const endTimestep = x.invert(selection[1]);
+        const importantWidthScale = d3
+            .scaleLinear()
+            .range([minimumChartWidth, width - MARGIN.right])
+            .domain([0, d3.max(iChunks, (d) => d.slice(extents[0], extents[1]).size)]);
 
-        const states = chunk.sequence
-            .map((sID) => GlobalStates.get(sID))
-            .map((s, i) => ({
-                timestep: chunk.timestep + i,
-                id: s.id,
-            }))
-            .filter(
-                (d) =>
-                    d.timestep >= Math.trunc(startTimestep) && d.timestep <= Math.trunc(endTimestep)
-            );
+        const getWidthScale = (data) => {
+            if (data.important) {
+                return importantWidthScale(data.slice(extents[0], extents[1]).size);
+            }
+            return unimportantWidthScale(data.size);
+        };
 
-        setExtents(
-            states,
-            trajectory.name,
-            { start: startTimestep, end: endTimestep },
-            { start: selection[0], end: selection[1] }
-        );
-    };
+        const totalSum = d3.sum(topChunkList, (d) => getWidthScale(d));
 
-    return (
-        <svg
-            className="vis"
-            id={`${trajectory.name}_sequence`}
-            ref={ref}
-            viewBox={[0, 0, width, height]}
-            onClick={() => {
-                if (trajectorySelectionMode) {
-                    selectObject(trajectory);
-                }
-            }}
-        >
-            {topChunkList.map((chunk) => {
-                const chunkIndex = topChunkList.indexOf(chunk);
-                const slicedChunk = chunk.slice(extents[0], extents[1]);
+        const scaleX = (w) => {
+            // given a width, scale it down so that it will fit within 1 screen
+            const per = w / totalSum;
+            return per * (width - MARGIN.right);
+        };
 
+        const getX = (i, w) => {
+            if (i > 0) {
+                const d = topChunkList[i - 1];
+                const wl = scaleX(getWidthScale(d));
+                return getX(i - 1, w + wl);
+            }
+            return w;
+        };
+        setScales({ getX, getWidthScale, scaleX });
+    }, [JSON.stringify(charts), width]);
+
+    useEffect(() => {
+        if (scales) {
+            const { topChunkList } = charts;
+            const { extents } = run;
+
+            // fill this dict with selection data for each chunk
+            const chartSelectionDict = {};
+            for (const chunk of topChunkList) {
+                const { scaleX, getWidthScale } = scales;
                 const chartW = scaleX(getWidthScale(chunk));
+
+                const slicedChunk = chunk.slice(extents[0], extents[1]);
                 const { values, current } = selections;
 
-                const chartSelections = Object.keys(values)
+                const chartSel = Object.keys(values)
                     .filter((selectionID) => {
                         const selection = values[selectionID];
                         const timesteps = selection.extent.map((d) => d.timestep);
@@ -216,80 +180,148 @@ export default function TrajectoryChart({
                             brushValues: { start: x(start), end: x(end) },
                         };
                     });
+                chartSelectionDict[chunk.id] = chartSel;
+            }
+            setChartSelections(chartSelectionDict);
+        }
+    }, JSON.stringify(selections));
 
-                const h = chunk.important ? height : height - scatterplotHeight;
+    // here we can filter out the un-rendered charts right away since we only care about rendering here
+    const finishBrush = (chunk, { selection }, chartWidth) => {
+        // extents determines the zoom level
+        const { extents } = run;
+        const [chunkSliceStart, chunkSliceEnd] = extents;
 
-                return (
-                    <foreignObject
-                        key={chunk.id}
-                        x={getX(chunkIndex, 0, topChunkList, scaleX, getWidthScale) + MARGIN.right}
-                        y={0}
-                        width={chartW}
-                        height={h}
-                    >
-                        <EmbeddedChart
-                            height={h}
+        const x = d3
+            .scaleLinear()
+            .domain(
+                d3.extent(chunk.timesteps.filter((d) => d >= chunkSliceStart && d <= chunkSliceEnd))
+            )
+            .range([0, chartWidth - 7.5]); // 7.5 to match margin on scatterplot
+
+        const startTimestep = x.invert(selection[0]);
+        const endTimestep = x.invert(selection[1]);
+
+        const states = chunk.sequence
+            .map((sID) => GlobalStates.get(sID))
+            .map((s, i) => ({
+                timestep: chunk.timestep + i,
+                id: s.id,
+            }))
+            .filter(
+                (d) =>
+                    d.timestep >= Math.trunc(startTimestep) && d.timestep <= Math.trunc(endTimestep)
+            );
+
+        setExtents(
+            states,
+            trajectory.name,
+            { start: startTimestep, end: endTimestep },
+            { start: selection[0], end: selection[1] }
+        );
+    };
+
+    const { topChunkList } = charts;
+    const { extents } = run;
+
+    return (
+        <svg
+            className="vis"
+            id={`${trajectory.name}_sequence`}
+            ref={ref}
+            viewBox={[0, 0, width, height]}
+            onClick={() => {
+                if (trajectorySelectionMode) {
+                    selectObject(trajectory);
+                }
+            }}
+        >
+            {scales &&
+                topChunkList.map((chunk) => {
+                    const { scaleX, getWidthScale, getX } = scales;
+                    const chartW = scaleX(getWidthScale(chunk));
+
+                    const chunkIndex = topChunkList.indexOf(chunk);
+                    const h = chunk.important ? height : height - scatterplotHeight;
+
+                    return (
+                        <foreignObject
+                            key={chunk.id}
+                            x={
+                                getX(chunkIndex, 0, topChunkList, scaleX, getWidthScale) +
+                                MARGIN.right
+                            }
+                            y={0}
                             width={chartW}
-                            color={chunk.color}
-                            brush={
-                                chunk.important
-                                    ? d3.brushX().on('end', (e) => finishBrush(chunk, e, chartW))
-                                    : undefined
-                            }
-                            onChartClick={() => {
-                                if (chunkSelectionMode && !trajectorySelectionMode) {
-                                    selectObject(chunk);
-                                }
-                            }}
-                            id={`ec_${chunk.id}`}
-                            selected={
-                                chunkSelectionMode &&
-                                !trajectorySelectionMode &&
-                                selectedObjects.map((d) => d.id).includes(chunk.id)
-                            }
-                            selections={chartSelections}
+                            height={h}
                         >
-                            {(ww, hh) =>
-                                chunk.important ? (
-                                    <ChunkWrapper
-                                        chunk={chunk}
-                                        width={ww}
-                                        height={hh}
-                                        setStateHovered={setStateHovered}
-                                        properties={properties}
-                                        trajectory={trajectory}
-                                        run={run}
-                                        globalScale={globalScale}
-                                        updateGlobalScale={updateGlobalScale}
-                                        updateRanks={updateRanks}
-                                        ranks={ranks.ordered.slice(0, showTop)}
-                                        selections={chartSelections}
-                                        showStateClustering={showStateClustering}
-                                        showTop={showTop}
-                                        doubleClickAction={() => expand(chunk.id, 100, trajectory)}
-                                        propertyCombos={propertyCombos}
-                                        extents={extents}
-                                        scatterplotHeight={scatterplotHeight}
-                                    />
-                                ) : (
-                                    <ViolinPlotWrapper
-                                        chunk={chunk}
-                                        width={ww}
-                                        height={hh} // to accomodate for no scatterplot
-                                        updateRanks={updateRanks}
-                                        ranks={ranks.ordered.slice(0, showTop)}
-                                        properties={properties}
-                                        globalScale={globalScale}
-                                        updateGlobalScale={updateGlobalScale}
-                                        showTop={showTop}
-                                        propertyCombos={propertyCombos}
-                                    />
-                                )
-                            }
-                        </EmbeddedChart>
-                    </foreignObject>
-                );
-            })}
+                            <EmbeddedChart
+                                height={h}
+                                width={chartW}
+                                color={chunk.color}
+                                brush={
+                                    chunk.important
+                                        ? d3
+                                              .brushX()
+                                              .on('end', (e) => finishBrush(chunk, e, chartW))
+                                        : undefined
+                                }
+                                onChartClick={() => {
+                                    if (chunkSelectionMode && !trajectorySelectionMode) {
+                                        selectObject(chunk);
+                                    }
+                                }}
+                                id={`ec_${chunk.id}`}
+                                selected={
+                                    chunkSelectionMode &&
+                                    !trajectorySelectionMode &&
+                                    selectedObjects.map((d) => d.id).includes(chunk.id)
+                                }
+                                selections={chartSelections[chunk.id]}
+                            >
+                                {(ww, hh) =>
+                                    chunk.important ? (
+                                        <ChunkWrapper
+                                            chunk={chunk}
+                                            width={ww}
+                                            height={hh}
+                                            setStateHovered={setStateHovered}
+                                            properties={properties}
+                                            trajectory={trajectory}
+                                            run={run}
+                                            globalScale={globalScale}
+                                            updateGlobalScale={updateGlobalScale}
+                                            updateRanks={updateRanks}
+                                            ranks={ranks.ordered.slice(0, showTop)}
+                                            selections={chartSelections}
+                                            showStateClustering={showStateClustering}
+                                            showTop={showTop}
+                                            doubleClickAction={() =>
+                                                expand(chunk.id, 100, trajectory)
+                                            }
+                                            propertyCombos={propertyCombos}
+                                            extents={extents}
+                                            scatterplotHeight={scatterplotHeight}
+                                        />
+                                    ) : (
+                                        <ViolinPlotWrapper
+                                            chunk={chunk}
+                                            width={ww}
+                                            height={hh} // to accomodate for no scatterplot
+                                            updateRanks={updateRanks}
+                                            ranks={ranks.ordered.slice(0, showTop)}
+                                            properties={properties}
+                                            globalScale={globalScale}
+                                            updateGlobalScale={updateGlobalScale}
+                                            showTop={showTop}
+                                            propertyCombos={propertyCombos}
+                                        />
+                                    )
+                                }
+                            </EmbeddedChart>
+                        </foreignObject>
+                    );
+                })}
             <rect
                 x={0}
                 y={0}
