937f544cc920c5c6f621fcd602a0236614445f3a
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2023-03-06 16:01:33 -0700

    Add loading bar to single state view

--- frontend/src/components/SingleStateViewer.jsx
+++ frontend/src/components/SingleStateViewer.jsx
@@ -1,6 +1,7 @@
 import { React, useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 import GlobalStates from '../api/globalStates';
 import ImageViewer from './ImageViewer';
 
@@ -8,14 +9,15 @@ import { apiGenerateOvitoImage } from '../api/ajax';
 
 export default function SingleStateViewer({ stateID, visScript, onClick }) {
     const [img, setImg] = useState(undefined);
-
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         const controller = new AbortController();
-
+        setIsLoading(true);
         apiGenerateOvitoImage(stateID, visScript, controller)
             .then((data) => {
                 GlobalStates.addPropToState(data);
                 setImg(data.img);
+                setIsLoading(false);
             })
             .catch(() => {});
         return () => controller.abort();
@@ -23,6 +25,7 @@ export default function SingleStateViewer({ stateID, visScript, onClick }) {
 
     return (
         <Box onClick={onClick}>
+            {isLoading && <LinearProgress variant="indeterminate" />}
             <ImageViewer img={img} />
         </Box>
     );
