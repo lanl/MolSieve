d09df1818e0adb48e345c2144f059b784f729b0a
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2023-03-17 18:01:31 -0700

    Use transition effect

--- frontend/src/hooks/useTrajectoryChartRender.js
+++ frontend/src/hooks/useTrajectoryChartRender.js
@@ -1,15 +1,21 @@
-import { startTransition, useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
+import { useTransitionEffect } from 'use-transition-effect';
 
 /* eslint-disable */
 export const useTrajectoryChartRender = (renderChartFn, dependencies) => {
     const ref = useRef();
 
+    const [_, startTransitionEffect, stopTransitionEffect] = useTransitionEffect();
+
     useEffect(() => {
         const svg = d3.select(ref.current); 
-        startTransition(() => { 
-            renderChartFn(svg)
+        startTransitionEffect(function* () { 
+            renderChartFn(svg);
+            yield;
         });
+
+        return () => stopTransitionEffect();
     }, dependencies);
 
     return ref;
