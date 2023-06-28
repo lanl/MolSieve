092b50b0c92b7346002072c041c4d85f0638df19
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2023-06-28 14:44:07 -0700

    update neomd

--- api/config.py
+++ api/config.py
@@ -1,8 +1,8 @@
 class Config(object):
     NEO4J_ADDRESS = "bolt://127.0.0.1:7687"
     NEO4J_AUTH = ("neo4j", "secret")
-    SAVE_CACHE = False
-    LOAD_CACHE = False
+    SAVE_CACHE = True
+    LOAD_CACHE = True
     SIZE_THRESHOLD = 250
 
 
